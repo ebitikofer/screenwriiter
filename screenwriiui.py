1af82ec06cf7ce685e2bd2619a79c6a50f888672
Eric Bitikofer <e.bitikofer@gmail.com>
2017-04-26 22:28:58 -0400

    load and saving from files now supported as well as new file, and compose saves file

--- screenwriiui.py
+++ screenwriiui.py
@@ -6,9 +6,6 @@ import screenwriiter
 
 qtCreatorFile = "gui/screenwriiter.ui" # Enter file here.
 
-entries = []
-selected = 0
-
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
 class screenwriiter_gui(QtGui.QMainWindow, Ui_MainWindow):
@@ -17,21 +14,31 @@ class screenwriiter_gui(QtGui.QMainWindow, Ui_MainWindow):
       QtGui.QMainWindow.__init__(self)
       Ui_MainWindow.__init__(self)
       self.setupUi(self)
-      self.load()
-      self.block_list.currentItemChanged.connect(self.edit)
+      self.action_New.triggered.connect(self.new)
+      self.action_Open.triggered.connect(self.load)
+      self.action_Save_as.triggered.connect(self.save)
+      self.blocks.currentItemChanged.connect(self.edit)
       self.compose_button.clicked.connect(self.update)
-      self.plus_button.clicked.connect(self.add_block)
-      self.minus_button.clicked.connect(self.sub_block)
+      self.plus_button.clicked.connect(self.add)
+      self.minus_button.clicked.connect(self.sub)
       self.compose_button.clicked.connect(self.compose)
+      self.entries = []
+
+
+  def new(self):
+    self.entries = []
+    self.update()
+    self.text_box.setPlainText("TEXT")
 
   def load(self):
     inf = open("save/save.txt", 'r')
+    self.entries = []
     entry = []
     store = False
     for line in inf:
       if store is True:
         entry.append(line[:-1])
-        entries.append(entry)
+        self.entries.append(entry)
         store = False
         entry = []
       elif line[0:3] == '-->':
@@ -41,36 +48,41 @@ class screenwriiter_gui(QtGui.QMainWindow, Ui_MainWindow):
     inf.close()
     self.update()
 
-  def update(self):
-    self.block_list.clear()
+  def save(self):
     outf = open("txtsrc/body.txt", 'w')
-    for entry in entries:
-      item = QtGui.QListWidgetItem(entry[0][3:-3])
-      self.block_list.addItem(item)
+    for entry in self.entries:
       outf.write(entry[0] + "\n")
       outf.write(entry[1] + "\n")
       outf.write("\n")
     outf.close()
     #update list
 
-  def edit(self):
-    self.text_box.setPlainText(entries[self.block_list.currentRow()][1])
+  def update(self):
+    self.blocks.clear()
+    for entry in self.entries:
+      item = QtGui.QListWidgetItem(entry[0][3:-3])
+      self.blocks.addItem(item)
     #update list
 
-  def add_block(self):
+  def edit(self):
+    self.text_box.setPlainText(self.entries[self.blocks.currentRow()][1])
+    #update list
+
+  def add(self):
     entry = []
     #i = selected + 1 #or last if none selected maybr use insert into list
-    entry.append(str(self.type_box.currentText()))
+    entry.append(r'-->' + str(self.type_box.currentText()) + r'<--')
     entry.append(str(self.text_box.toPlainText()))
-    entries.insert(self.block_list.currentRow(), entry)
+    self.entries.insert(self.blocks.currentRow(), entry)
     self.text_box.setPlainText("TEXT")
     self.update()
 
-  def sub_block(self):
-    entries.pop(self.block_list.currentRow())
+  def sub(self):
+    self.entries.pop(self.blocks.currentRow())
     self.update()
 
   def compose(self):
+    self.save()
     with open('out.tex', 'w') as f:
         subprocess.call(['python3', 'screenwriiter.py'], stdout=f)
     subprocess.run(['pdflatex', 'out.tex'])
